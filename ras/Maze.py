ccf6c033121d18831bc12c86bb6406c8afb77faa
yellowoov <yellowoov@kookmin.ac.kr>
2017-12-13 11:20:19 +0900

    Update Maze.py

--- ras/Maze.py
+++ ras/Maze.py
@@ -10,6 +10,17 @@ speed = 15
 
 
 def lineTrace():
+    '''
+    라인 트레이싱을 하기 위한 lineTrace함수입니다.
+    led_list라는 변수에 Tracking모듈에서 리턴받은 튜플 값을 받아옵니다.
+
+    5방향 센서의 5개 센서를 왼쪽부터 0,1,2,3,4번이라고 했을 때,
+    0번이 True이고 4번이 False이면 왼쪽 바퀴의 속도를 올려 오른쪽으로 턴하고
+    1번이 True이고 3번이 False이면 왼쪽 바퀴의 속도를 올려 오른쪽으로 턴합니다.
+
+    3번이 True이고 1번이 False이면 오른쪽 바퀴의 속도를 올려 왼쪽으로 턴하고
+    4번이 True이고 0번이 False이면 오른쪽 바퀴의 속도를 올려 왼쪽으로 턴합니다.
+    '''
     go_forward_nosleep(40)
     while True:
         try:
@@ -36,6 +47,23 @@ def lineTrace():
 
 
 def mazeSearch(led_list):
+    '''
+    미로 찾기를 하기 위한 mazeSearch 함수입니다.
+    
+    5방향 센서의 5개 센서를 왼쪽부터 0,1,2,3,4번이라고 했을 때,
+    모두 True일 때, 유턴을 합니다.
+    
+    0번과 1번이 True, 3번과 4번이 False일 때,
+    오른쪽으로 턴합니다.
+    
+    0번과 1번이 False, 3번과 4번이 True일 때,
+    왼쪽으로 턴합니다.
+    
+    모두 False일 때, 우수법 때문에
+    오른쪽으로 턴합니다.
+    
+    모든 턴은 진행하기 전에 조금 앞으로 전진했다가 턴합니다.
+    '''
     try:
         if led_list[0] and led_list[1] and led_list[2] and led_list[3] and led_list[4]:
             print("u")
